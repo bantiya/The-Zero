ea641bed2b5b6ebfabc28ee864336047f9b05d65
Bhavya Antiya <bhavyaantiya@gmail.com>
2024-02-18 11:42:12 -0500

    First commit

---
+++ Application/MyFirstEcommerceWeb/src/main/java/com/zero/MyFirstEcommerceWeb/MyFirstEcommerceWebApplication.java
@@ -0,0 +1,13 @@
+package com.zero.MyFirstEcommerceWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstEcommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstEcommerceWebApplication.class, args);
+	}
+
+}
---
+++ Application/MyFirstEcommerceWeb/src/test/java/com/zero/MyFirstEcommerceWeb/MyFirstEcommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.MyFirstEcommerceWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstEcommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
